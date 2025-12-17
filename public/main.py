cd9b8626742821da50e773968e6a8e599c4effe4
SchmaloDev <m.dinter06@gmail.com>
2025-12-17 10:17:42 +0000

    feat backend convert to java

--- public/main.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template
-
-app = Flask(__name__, static_folder="../src/view/static", template_folder="../src/view/templates")
-
-@app.route("/stand", methods=["POST", "GET"])
-def show_stand() -> render_template:
-    return render_template("stand.html")
-
-app.run(host="0.0.0.0", port=8000)
